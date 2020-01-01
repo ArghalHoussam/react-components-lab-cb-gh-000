f9d78731eac57e4d92a47c57a9c3bb05fc177f7e
ossama abd rahman <houssam.arghal1997@gmail.com>
2020-01-01 10:09:15 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -18,14 +18,42 @@ export class OlderCoaster extends Component {
   //   )
   // }
   //
+  render() {
+    return (
+      <div class="OlderCoaster">
+        <p>Two grannies having the tile of their life!</p>
+        <p>Passengers:</p>
+        <ul>
+          <li>Agnes</li>
+          <li>Muriel</li>
+        </ul>
+      </div>
+    )
+  }
 }
 
 export class InFrontOfYou extends Component {
   // your code here
+  render() {
+    return (
+      <div>
+        <p>{"You shouldn't look too far."}</p>
+        <p>Sometimes, the solution is right in front of you.</p>
+      </div>
+    )
+  }
 }
+
+let products = BUTCHER_PRODUCTS.map( product => <li> { product } </li>);
 
 export class ButcherShop extends Component {
   // your code here
+  <div class="butcher-shop">
+  <p>Hello! We have the following products for sale today:</p>
+  <ul>
+    { products }
+  </ul>
+</div>
 }
 
 
@@ -33,7 +61,9 @@ export class App extends Component {
   render() {
     return (
       <div id="app">
-
+          <OlderCoaster />
+          <InFrontOfYou />
+          <ButcherShop />
       </div>
     )
   }
